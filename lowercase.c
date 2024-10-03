8a98f774422be7d315c373d8c477ae7389a49592
TinaKB <kec047@ucsd.edu>
2024-10-03 15:39:29 -0700

    Update lowercase.c

--- lowercase.c
+++ lowercase.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+//Take the string and change upper to low for each char
 
 void inspect(char s[]) {
   int index = 0;
@@ -12,30 +13,31 @@ void inspect(char s[]) {
 }
 
 char lower(char c) {
-
-
-
-  
-}
+  if c >= 'A' && c <= 'Z'){
+    return c+32; 
+    //return c|0b00100000
+    // bit 
+  }
+} return c;
 
 void lowercase(char s[]) {
   int index = 0;
 
-  while(                      ) {
-
+  while(s[index] != 0   ) {
     s[index] = lower(s[index]);
     index += 1;
   }
 }
+// forloop  for(int index - 0;s[index]!=0;index++1)
 
 int main() {
   char abc[] = "ABC";
   lowercase(abc);
-  inspect(abc);
+  inspect(abc); //expect a(97), b(98), c(99)
 
   char mixed[] = "Hello Aaron!";
   lowercase(mixed);
-  inspect(mixed);
+  inspect(mixed); //expect h(104)...
 
 
 
